cd707bd48e16750c0c8d50de75975125a8e2ee2c
anmoltyag <tyagi1558@gmail.com>
2023-07-28 00:59:16 +0530

    netflix

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, {useEffect} from 'react';
+import './App.css';
+import HomeScreen from './screen/HomeScreen';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
+import LoginScreen from './screen/LoginScreen';
+import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux'
+import { login, logout, selectUser } from './features/userSlice';
+import ProfileScreen from './screen/ProfileScreen';
+
+function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if(userAuth){
+        console.log(userAuth);
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email
+        })
+        );
+      }else{
+        dispatch(logout());
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    }
+  }, [dispatch]);
+
+  return (
+    <div className="app">
+      <Router>
+        {!user ? 
+          (<LoginScreen />) : (
+        <Switch>
+          <Route path='/profile'>
+            <ProfileScreen />  
+          </Route>
+          <Route path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+        )}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
